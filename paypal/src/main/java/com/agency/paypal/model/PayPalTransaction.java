c52e62cec41e430e1fbb965373cc46d516b5e154
Milica <sammilica99@gmail.com>
2022-12-19 20:09:25 +0100

    fix: compile error fixed

--- paypal/src/main/java/com/agency/paypal/model/PayPalTransaction.java
+++ paypal/src/main/java/com/agency/paypal/model/PayPalTransaction.java
@@ -4,13 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import java.time.LocalDateTime;
+
+import javax.persistence.*;
 
 @Entity
 @Setter
