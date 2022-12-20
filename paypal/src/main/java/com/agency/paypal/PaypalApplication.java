79c0698fe18732d8e2428ed6dd777cf8b580a4a4
nevenaatic <74498373+nevenaatic@users.noreply.github.com>
2022-12-20 15:23:38 +0100

    feat: created one log file for all logs level

--- paypal/src/main/java/com/agency/paypal/PaypalApplication.java
+++ paypal/src/main/java/com/agency/paypal/PaypalApplication.java
@@ -1,13 +1,15 @@
 package com.agency.paypal;
-
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PaypalApplication {
+	final static Logger log = Logger.getLogger(PaypalApplication.class.getName());
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaypalApplication.class, args);
+		log.info("Hello from PayPalApp! Welcome! :)");
 	}
 
 }
